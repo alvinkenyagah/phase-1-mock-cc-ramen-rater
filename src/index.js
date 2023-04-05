483c44557ec6d5f52c4b461da8372b5592ef1d1c
alvinkenyagah <alvin47k@gmail.com>
2023-04-05 12:30:19 +0300

    Working on the fetch method

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-// write your code here
+document.addEventListener("DOMContentLoaded", function() {
+
+
+
+let form = document.querySelector('form');
+
+let name=document.getElementById('new-name');
+let restaurant = document.getElementById('new-restaurant');
+let image = document.getElementById('new-image');
+let rating = document.getElementById('new-rating');
+let comment = document.getElementById('new-comment');
+
+
+
+
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    fetch('http://localhost:3001/ramens')
+    .then(response => response.json())
+    .then(data => {
+
+        for(let i = 0 ;i < data.length;i++) {
+
+let image = document.createElement('img');
+image.src = data[]
+
+
+
+        }
+
+
+
+
+
+
+    })
+
+
+
+})
+
+
+})
